831c8c06ff4db8c570932ec64e98bd59d0c4bf62
obwansan <kob123@hotmail.co.uk>
2018-03-23 11:33:37 +0000

    Added doc block to checkCredentials()

--- loginFunctions.php
+++ loginFunctions.php
@@ -2,15 +2,19 @@
 
 session_start();
 
+
 $correctUsername = 'obwansan';
 $correctPassword = 'turnip100';
 
 /**
  * Checks if username and password are correct
  *
- * @param $username, $password, $correctUsername, $correctPassword string
+ * @param $username string to be checked against $correctUsername
+ * @param $password string to be checked against $correctPassword
+ * @param $correctUsername string to be checked against $username
+ * @param $correctPassword string to be checked against $password
  *
- * @return boolean
+ * @return true if username & passwords match / session logged in, and false if not
  */
 function checkCredentials($username, $correctUsername, $password, $correctPassword) {
 
