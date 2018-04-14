2e665fb4506d1ebabdf059534ea9d18cb916b137
obwansan <kob123@hotmail.co.uk>
2018-04-14 19:20:37 +0100

    portfolio page bootstrap responsive

--- portfolio.php
+++ portfolio.php
@@ -19,43 +19,43 @@
 <body class="portfolio-BG">
     <div class="container">
         <h1>Portfolio</h1>
-        <div class="gallery">
-            <div class="grid-column">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                 <a href="https://dev.maydenacademy.co.uk/students/2018/kevin/mayden-logo/" target="_blank"><img class="portfolio-image" src="images/mayden-logo.png" alt="Mayden logo"></a>
                 <p class="projectDesc">Mayden logo made with HTML & CSS</p>
                 <a href="https://github.com/obwansan/mayden-logo-challenge" target="_blank" class="viewCode">&lt;view code&gt;</a>
             </div>
-            <div class="grid-column">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                 <a href="https://dev.maydenacademy.co.uk/students/2018/kevin/pilot-shop-build/" target="_blank"><img class="portfolio-image" src="images/pilot-shop.png" alt="Pilot Shop website logo"></a>
                 <p class="projectDesc">Clone of ecommerce website</p>
                 <a href="https://github.com/obwansan/pilot-shop-responsive-site" target="_blank" class="viewCode">&lt;view code&gt;</a>
             </div>
-            <div class="grid-column">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                 <a href="#"><img class="portfolio-image" src="images/jsGame-mayrioKart.png"></a>
                 <p class="projectDesc">JavaScript game - team project</p>
                 <a href="https://github.com/Mayden-Academy/2018JSGame" target="_blank" class="viewCode">&lt;view code&gt;</a>
             </div>
-            <div class="grid-column">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                 <a href="https://dev.maydenacademy.co.uk/students/2018/kevin/postsRailings/" target="_blank"><img class="portfolio-image" src="images/posts-rails.png"></a>
                 <p class="projectDesc">PHP posts & railings challenge</p>
                 <a href="https://github.com/obwansan/posts-and-railings-challenge" target="_blank" class="viewCode">&lt;view code&gt;</a>
             </div>
-            <div class="grid-column">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                 <a href="#"><img class="portfolio-image" src="images/portfolio-site.png"></a>
                 <p class="projectDesc">Portfolio and CMS</p>
                 <a href="https://github.com/obwansan/mayden-portfolio/tree/story-cms" target="_blank" class="viewCode">&lt;view code&gt;</a>
             </div>
-            <div class="grid-column">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                 <a href="#"><img class="portfolio-image" src="images/ace-bet-blackjack.jpg"></a>
                 <p class="projectDesc">PHP blackjack game & function tests</p>
                 <a href="https://github.com/obwansan/blackjack-game" target="_blank" class="viewCode">&lt;view code&gt;</a>
             </div>
-            <div class="grid-column">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                 <a href="https://dev.maydenacademy.co.uk/students/2018/kevin/formValidatorChallenge/"><img class="portfolio-image" src="images/form-validation.png"></a>
                 <p class="projectDesc">JavaScript form validation challenge</p>
                 <a href="https://github.com/obwansan/form-validator-challenge/" target="_blank" class="viewCode">&lt;view code&gt;</a>
             </div>
-            <div class="grid-column">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                 <a href="https://dev.maydenacademy.co.uk/students/2018/kevin/timerChallenge/"><img class="portfolio-image" src="images/project-placeholder.png"></a>
                 <p class="projectDesc">JS DOM - Timer challenge</p>
                 <a href="https://github.com/obwansan/timer-challenge" target="_blank" class="viewCode">&lt;view code&gt;</a>
