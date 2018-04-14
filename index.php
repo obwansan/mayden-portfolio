b135f05e1691b8907ef92319951db89cf6cf2c60
obwansan <kob123@hotmail.co.uk>
2018-04-14 21:38:29 +0100

    bootstrap navbar working on home page

--- index.php
+++ index.php
@@ -13,11 +13,35 @@
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
     <!--  JS  -->
     <script type="text/javascript" src="js/animations.js" defer></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" src="css/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
 </head>
 <!--[if IE]><body class="ie"><![endif]-->
+
+<!-- nav bar -->
+<nav class="navbar transparent navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-home"></span> </a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-folder-open"></span></a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- main -->
 <body class="home-BG">
     <div class="container">
-        <div class="row"></div>
+        <div class="row">
             <div class="col-lg-12 homePageText">Where am I?</div>
             <div class="overlay">
                 <div class="text">Overlay Text</div>
