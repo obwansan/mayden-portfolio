085b9899c4bf6e75110512db21f57914b811ef98
obwansan <kob123@hotmail.co.uk>
2018-05-03 09:45:59 +0100

    updated PDO with password

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 
 require_once 'cms/profileFunctions.php';
 
-$db = new PDO('mysql:host=127.0.0.1;dbname=portfolio-kevin', 'root');
+$db = new PDO('mysql:host=127.0.0.1;dbname=portfolio-kevin', 'root', 'LxM7<{9d1AMR');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 ?>
